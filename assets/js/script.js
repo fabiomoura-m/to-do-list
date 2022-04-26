4dccc7af4eac9f2e394a271fcd3869c2cc004058
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2022-04-26 08:28:12 -0300

    feat: inclui arrasta e solta das tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,8 +19,33 @@ function addTask(element) {
   document.querySelectorAll('.remove').forEach(item => {
     item.addEventListener('click', removeTask);
   });
+  document.querySelectorAll('li').forEach(item => {
+    item.addEventListener('dragstart', dragStart);
+    item.setAttribute('draggable', 'true');
+  });
+  document.querySelectorAll('ul').forEach(item => {
+    item.addEventListener('dragover', dragOver);
+    item.addEventListener('drop', drop);
+  });
 }
 
 function removeTask(element) {
-  element.currentTarget.parentElement.remove();
+  element.currentTarget.parentElement.remove(); // remove o pai do elemento clicado
+}
+
+function dragStart(event) {
+  event.dataTransfer.setData('task', event.target.id); // salvando o elemento ao arrastar, neste caso salvado o id do li
+}
+
+function dragOver(event) {
+  event.preventDefault();
+}
+
+function drop(event) {
+  event.preventDefault(); // previne a ação padrão que é de negar o drop
+  const target = event.target;
+  const data = event.dataTransfer.getData('task');
+  const task = document.getElementById(data);
+  target.appendChild(task);
+  event.dataTransfer.clearData(); // apagar da memória o elemento
 }
